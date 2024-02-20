720b98662df9c9cd36575477756519dfe16b7a79
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-02-21 01:24:59 +0800

    Update ExternalControlActivity.kt

--- app/src/main/java/com/github/kr328/clash/ExternalControlActivity.kt
+++ app/src/main/java/com/github/kr328/clash/ExternalControlActivity.kt
@@ -9,9 +9,7 @@ import com.github.kr328.clash.common.constants.Intents
 import com.github.kr328.clash.common.util.intent
 import com.github.kr328.clash.common.util.setUUID
 import com.github.kr328.clash.design.MainDesign
-import com.github.kr328.clash.design.ui.ToastDuration
 import com.github.kr328.clash.remote.Remote
-import com.github.kr328.clash.remote.StatusClient
 import com.github.kr328.clash.service.model.Profile
 import com.github.kr328.clash.util.startClashService
 import com.github.kr328.clash.util.stopClashService
@@ -22,88 +20,68 @@ import kotlinx.coroutines.launch
 import java.util.*
 
 class ExternalControlActivity : Activity(), CoroutineScope by MainScope() {
-
-    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        // Handle the results of PropertiesActivity here if necessary.
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         when (intent.action) {
             Intent.ACTION_VIEW -> handleActionView()
-            Intents.ACTION_TOGGLE_CLASH -> toggleClash()
-            Intents.ACTION_START_CLASH -> startClash()
-            Intents.ACTION_STOP_CLASH -> stopClash()
+            Intents.ACTION_TOGGLE_CLASH -> handleToggleClash()
+            Intents.ACTION_START_CLASH -> handleStartClash()
+            Intents.ACTION_STOP_CLASH -> handleStopClash()
+        }
+    }
+
+    private fun handleActionView() {
+        val uri = intent.data ?: return finish()
+        val url = uri.getQueryParameter("url") ?: return finish()
+
+        launch {
+            val type = when (uri.getQueryParameter("type")?.lowercase(Locale.getDefault())) {
+                "url" -> Profile.Type.Url
+                "file" -> Profile.Type.File
+                else -> Profile.Type.Url
+            }
+            val name = uri.getQueryParameter("name") ?: getString(R.string.new_profile)
+
+            withProfile {
+                create(type, name).also {
+                    it.patch(name, url, 0)
+                    startActivity(PropertiesActivity::class.intent.setUUID(it.uuid))
+                }
+            }
         }
         finish()
     }
 
-    private fun handleActionView() {
-        val uri = intent?.data ?: return
-        val url = uri.getQueryParameter("url") ?: return
-
-        launch {
-            val uuid = withProfile {
-                val type = when (uri.getQueryParameter("type")?.lowercase(Locale.getDefault())) {
-                    "url" -> Profile.Type.Url
-                    "file" -> Profile.Type.File
-                    else -> Profile.Type.Url
-                }
-                val name = uri.getQueryParameter("name") ?: getString(R.string.new_profile)
-
-                create(type, name).also {
-                    patch(it, name, url, 0)
-                }
-            }
-            val intent = PropertiesActivity::class.intent.setUUID(uuid)
-            startForResult.launch(intent)
-        }
-    }
-
-    private fun toggleClash() {
+    private fun handleToggleClash() {
         if (Remote.broadcasts.clashRunning) {
-            stopClashServiceWithToast()
+            handleStopClash()
         } else {
-            startClashServiceWithToast()
+            handleStartClash()
         }
     }
 
-    private fun startClash() {
+    private fun handleStartClash() {
         if (!Remote.broadcasts.clashRunning) {
-            startClashServiceWithToast()
-        } else {
-            showToastIfRunning(R.string.external_control_started)
+            val vpnRequest = startClashService()
+            if (vpnRequest != null) {
+                showToast(R.string.unable_to_start_vpn)
+                return
+            }
         }
+        showToast(R.string.external_control_started)
+        finish()
     }
 
-    private fun stopClash() {
+    private fun handleStopClash() {
         if (Remote.broadcasts.clashRunning) {
-            stopClashServiceWithToast()
-        } else {
-            showToastIfRunning(R.string.external_control_stopped)
+            stopClashService()
         }
-    }
-
-    private fun startClashServiceWithToast() {
-        val vpnRequest = startClashService()
-        if (vpnRequest != null) {
-            showToast(R.string.unable_to_start_vpn)
-        } else {
-            showToast(R.string.external_control_started)
-        }
-    }
-
-    private fun stopClashServiceWithToast() {
-        stopClashService()
         showToast(R.string.external_control_stopped)
+        finish()
     }
 
     private fun showToast(messageResId: Int) {
         Toast.makeText(this, messageResId, Toast.LENGTH_LONG).show()
-    }
-
-    private fun showToastIfRunning(messageResId: Int) {
-        showToast(messageResId)
     }
 }
